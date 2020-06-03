7ef56388e5cc0c36d4674fded1a09b0d1e7a66a7
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-03 10:35:09 +0500

    promise userId set for default

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,11 @@
 const executer = (promise) => (req, res) => {
       promise(req)
       .then(({data, status}) => res.status(status).json(data))
-      .catch(({ response: {status, data} }) => res.status(status).json(data))
+      .catch(({ response: resp }) => res.status(resp ? resp.status: 502).json(resp ? resp.data : { message: 'Error' }))
 }
 
 const validate = (roleId) => (req, res, next) => {
-    if (req.user.data.roleId !== roleId) next();
+    if (req.user.roleId !== roleId) next();
     else res.status(400).json({ message: 'Not permitted' });
 };
 
