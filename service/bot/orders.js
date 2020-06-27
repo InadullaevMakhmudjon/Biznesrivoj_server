9f2d21c8c5ee202f15edf35c54e663c2101744bb
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-27 10:43:43 +0500

    Bot service endpoints created

--- service/bot/orders.js
+++ service/bot/orders.js
@@ -3,4 +3,6 @@ import API, { execute } from '.';
 export default {
   getAll: (req) => execute(API.GET, 'orders', req),
   get: (req) => execute(API.GET, `orders/${req.params.id}`, req),
+  waiting: (req) => execute(API.POST, `orders/${req.params.id}/waiting`, req),
+  done: (req) => execute(API.POST, `orders/${req.params.id}/done`, req),
 }
