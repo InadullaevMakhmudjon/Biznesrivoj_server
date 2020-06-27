609d5384a10573e0496748eb5b705e82826a9f5d
Makhmudjon <maxmudinadullayev98@gmail.com>
2020-06-27 10:43:59 +0500

    Bot update status update routes added

--- routes/bot-orders.js
+++ routes/bot-orders.js
@@ -7,5 +7,7 @@ const router = Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), executer(Orders.getAll));
 router.get('/:id', passport.authenticate('jwt', { session: false }), executer(Orders.get));
+router.post('/:id/waiting', passport.authenticate('jwt', { session: false }), executer(Orders.waiting));
+router.post('/:id/done', passport.authenticate('jwt', { session: false }), executer(Orders.done));
 
 export default router;
